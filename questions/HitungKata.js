d277aafe1e54ba2515c0c470da26cd1525449954
Irvansyah <ilhamirvansyah.dev@gmail.com>
2023-10-16 21:19:57 +0700

    menyelesaikan semua questions project javascript

--- questions/HitungKata.js
+++ questions/HitungKata.js
@@ -1,15 +1,25 @@
 /**
- * Buatlah sebuah fungsi Javascript yang bernama hitungKata($kalimat) yang akan menerima satu parameter berupa string $kalimat. 
- * Fungsi ini harus mengembalikan jumlah kata dalam kalimat tersebut. 
+ * Buatlah sebuah fungsi Javascript yang bernama hitungKata($kalimat) yang akan menerima satu parameter berupa string $kalimat.
+ * Fungsi ini harus mengembalikan jumlah kata dalam kalimat tersebut.
  * Kata-kata dalam kalimat akan dipisahkan oleh spasi.
  * Contoh:
  * Input: "Halo nama saya Galih"
  * Output: 4
  */
 
-// ! Jangan ubah nama function 
+// ! Jangan ubah nama function
 function hitungKata(kalimat) {
-    // Tulis kodemu disini
+  // Tulis kodemu disini
+  if (typeof kalimat !== "string") {
+    throw new Error("Input Harus Berupa String !");
+  }
+
+  if (kalimat.trim() === "") {
+    return 0;
+  }
+
+  const words = kalimat.split(/\s+/).filter((words) => words !== "");
+  return words.length;
 }
 
 module.exports = { hitungKata };
--- questions/HitungRataRata.js
+++ questions/HitungRataRata.js
@@ -5,9 +5,21 @@
  * Output: 78
  */
 
-// ! Jangan ubah nama function 
+// ! Jangan ubah nama function
 function hitungRataRata(nilai) {
-    // Tulis kodemu disini
+  // Tulis kodemu disini
+  if (!Array.isArray(nilai)) {
+    throw new Error("Input nilai harus berupa array");
+  }
+
+  // Mengembalikan null jika arraynya kosong
+  if (nilai.length === 0) {
+    return 0;
+  }
+
+  const total = nilai.reduce((sum, current) => sum + current, 0);
+  const average = total / nilai.length;
+  return Math.round(average);
 }
 
 module.exports = { hitungRataRata };
